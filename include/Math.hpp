704ead4a2d5a411d1e347f69ca2282f806aaccf2
Andrei-Edward Popa <andrei_edward.popa@upb.ro>
2021-09-09 16:54:43 +0300

    Added complex usage for math functions.
    Signed-off-by: Andrei-Edward Popa <andrei_edward.popa@upb.ro>

--- include/Math.hpp
+++ include/Math.hpp
@@ -6,16 +6,24 @@
 
 namespace cte::math {
 
-template<typename Type>
-constexpr Type abs(const Type value) noexcept {
-    return value == Type{} ? Type{} : value < Type{} ? - value : value;
+template<FloatingPoint FloatingPointType, Integer IntegerType>
+constexpr FloatingPointType pow(const FloatingPointType base, const IntegerType exponent) noexcept {
+    if (base == FloatingPointType{0}) return base;
+    FloatingPointType result = FloatingPointType{1};
+    FloatingPointType cbase = exponent < 0 ? 1 / static_cast<FloatingPointType>(base) : static_cast<FloatingPointType>(base);
+    for (IntegerType index = 0; index < abs(exponent); index++) {
+        result *= cbase;
+    }
+    return result;
 }
 
-template<typename Type, Integer IntegerType>
-constexpr long double pow(const Type base, const IntegerType exponent) noexcept {
-    if (base == Type{0}) return base;
-    long double result = 1.0;
-    long double cbase = exponent < 0 ? 1 / static_cast<long double>(base) : static_cast<long double>(base);
+template<Complex ComplexType, Integer IntegerType>
+constexpr ComplexType pow(const ComplexType base, const IntegerType exponent) noexcept {
+    using ComplexFloatingPointType = ComplexType::value_type;
+    if (base == ComplexType{0, 0}) return base;
+    ComplexType result{1, 0};
+    ComplexType cbase = exponent < 0 ? std::complex{1 / static_cast<ComplexFloatingPointType>(base.real()), 1 / static_cast<ComplexFloatingPointType>(base.imag())} 
+                                                   : std::complex{static_cast<ComplexFloatingPointType>(base.real()), static_cast<ComplexFloatingPointType>(base.imag())};
     for (IntegerType index = 0; index < abs(exponent); index++) {
         result *= cbase;
     }
@@ -28,6 +36,21 @@ constexpr FloatingPointType sqrt(FloatingPointType value) noexcept {
         return current == previous ? current : sqrtNewtonRaphson(sqrtNewtonRaphson, value, 0.5 * (current + value / current), current);
     };
     return value >= 0 && value < std::numeric_limits<FloatingPointType>::infinity() ? sqrtNewtonRaphson(sqrtNewtonRaphson, value, value, 0) : std::numeric_limits<FloatingPointType>::quiet_NaN();
+}
+
+template<Integer IntegerType>
+constexpr IntegerType abs(const IntegerType value) noexcept {
+    return value == IntegerType{} ? IntegerType{} : value < IntegerType{} ? - value : value;
+}
+
+template<FloatingPoint FloatingPointType>
+constexpr FloatingPointType abs(const FloatingPointType value) noexcept {
+    return value == FloatingPointType{} ? FloatingPointType{} : value < FloatingPointType{} ? - value : value;
+}
+
+template<Complex ComplexType>
+constexpr auto abs(const ComplexType value) noexcept -> ComplexType::value_type {
+    return cte::math::sqrt(static_cast<ComplexType::value_type>(value.real() * value.real() + value.imag() * value.imag()));
 }
 
 }
